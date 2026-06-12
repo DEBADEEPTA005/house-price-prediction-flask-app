c30662e8529d00f03fabda050160bf1e85a9d489
DEBADEEPTA005 <welcomedebadeepta@gmail.com>
2026-06-12 20:27:26 +0530

    App run set to true

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def predict():
     return render_template('index.html', prediction_text=f"Predicted Price: {prediction[0]:.2f}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
